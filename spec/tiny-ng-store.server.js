680658e46146239dc3958d7d82c3a3caaea5998a
JScearcy <jacobscearcy@gmail.com>
2016-06-22 14:12:35 -0500

    update for node compatibility

--- spec/tiny-ng-store.server.js
+++ spec/tiny-ng-store.server.js
@@ -1,6 +1,6 @@
-const spawn = require('child_process').spawn;
-let npm = (process.platform === "win32" ? "npm.cmd" : "npm");
-let liveServer = spawn(npm, ['run', 'test'], {detached: true});
+var spawn = require('child_process').spawn;
+var npm = (process.platform === "win32" ? "npm.cmd" : "npm");
+var liveServer = spawn(npm, ['run', 'test'], {detached: true});
 
 setTimeout(function() {
     spawn("taskkill", ["/pid", liveServer.pid, '/f', '/t']);
