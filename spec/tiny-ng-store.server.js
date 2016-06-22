ff68c560825970c4b384b03fae84b5f6f9199166
JScearcy <jacobscearcy@gmail.com>
2016-06-22 17:31:09 -0500

    Add additional build

--- spec/tiny-ng-store.server.js
+++ spec/tiny-ng-store.server.js
@@ -1,6 +1,6 @@
 const spawn = require('child_process').spawn;
 let npm = (process.platform === "win32" ? "npm.cmd" : "npm");
-let liveServer = spawn(npm, ['run', 'test'], {detached: true});
+let liveServer = spawn(npm, ['run', 'server-test'], {detached: true});
 
 setTimeout(function() {
     spawn("taskkill", ["/pid", liveServer.pid, '/f', '/t']);
