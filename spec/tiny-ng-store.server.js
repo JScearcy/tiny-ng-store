97cf48bcb5d32448d174fa91b362459e91b1cc5b
JScearcy <jacobscearcy@gmail.com>
2016-06-22 17:37:52 -0500

    update tests

--- spec/tiny-ng-store.server.js
+++ spec/tiny-ng-store.server.js
@@ -1,7 +1,11 @@
-const spawn = require('child_process').spawn;
-let npm = (process.platform === "win32" ? "npm.cmd" : "npm");
-let liveServer = spawn(npm, ['run', 'server-test'], {detached: true});
+var spawn = require('child_process').spawn;
+var npm = (process.platform === "win32" ? "npm.cmd" : "npm");
+var liveServer = spawn(npm, ['run', 'server-test'], {detached: true});
 
 setTimeout(function() {
-    spawn("taskkill", ["/pid", liveServer.pid, '/f', '/t']);
+    if (process.platform === "win32") {
+        spawn("taskkill", ["/pid", liveServer.pid, '/f', '/t']);
+    } else {
+        process.kill(-liveServer.pid, 'SIGTERM');
+    }
 }, 30000);
