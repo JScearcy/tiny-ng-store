a937e7a82b2966879f61a72f4cd21debfd769302
JScearcy <jacobscearcy@gmail.com>
2016-06-23 10:24:29 -0500

    update test runner

--- karma.conf.js
+++ karma.conf.js
@@ -6,20 +6,17 @@ module.exports = function(config) {
         frameworks: ['jasmine'],
  
         files: [
-            // paths loaded by Karma
             {pattern: 'node_modules/systemjs/dist/system.src.js', included: true, watched: true},
             {pattern: 'node_modules/zone.js/dist/zone.js', included: true, watched: true},
             {pattern: 'node_modules/reflect-metadata/Reflect.js', included: true, watched: true},
             {pattern: 'node_modules/rxjs/bundles/Rx.js', included: true, watched: true},
             {pattern: 'karma-test-shim.js', included: true, watched: true},
  
-            // paths loaded via module imports
             {pattern: 'tiny-ng-store.js', included: false, watched: true},
             {pattern: 'node_modules/@angular/core/**/*.js', included: false, watched: true},
             {pattern: 'node_modules/rxjs/**/*.js', included: false, watched: true},
             {pattern: 'spec/tiny-ng-store.spec.js', included: false, watched: true},
 
-            // paths to support debugginsg with source maps in dev tools
             {pattern: 'tiny-ng-store.ts', included: false, watched: false}
         ],
  
@@ -31,20 +28,16 @@ module.exports = function(config) {
  
         autoWatch: true,
  
-        browsers: ['Chrome'],
+        browsers: ['Firefox'],
  
-        // Karma plugins loaded
         plugins: [
             'karma-jasmine',
             'karma-coverage',
-            'karma-chrome-launcher'
+            'karma-firefox-launcher'
         ],
  
-        // Coverage reporter generates the coverage
         reporters: ['progress', 'dots', 'coverage'],
  
-        // Source files that you wanna generate coverage for.
-        // Do not include tests or libraries (these files will be instrumented by Istanbul)
         preprocessors: {
             '!(*spec).js': ['coverage']
         },
