7d7c43a86e904dd6daf8b55e8c8c089620ef0ff0
JScearcy <jacobscearcy@gmail.com>
2016-06-23 08:59:53 -0500

    update tests further

--- spec/tiny-ng-store.server.js
+++ spec/tiny-ng-store.server.js
@@ -1,11 +1,35 @@
 var spawn = require('child_process').spawn;
+var http = require('http');
+var pass = false
+var server = http.createServer(function (req, res) {
+    var status = parseInt(req.url.replace('/', ''));
+    if (status >= 0) {
+        status === 0 ? pass = true : pass = false;
+    }
+    res.writeHead(200, 
+        { 'Content-Type': 'text/plain'
+        , 'Access-Control-Allow-Origin': '*'
+        , 'Access-Control-Allow-Headers': 'X-Requested-With'
+        }
+    );
+    res.end();
+}).listen(3001);
+
 var npm = (process.platform === "win32" ? "npm.cmd" : "npm");
-var liveServer = spawn(npm, ['run', 'server-test'], {detached: true});
+var liveServer = spawn(npm, ['run', 'server-test'], {detached: true})
+liveServer.on('SIGTERM', function() {
+    console.log('closing');
+    process.exit(pass ? 0 : 1);
+});
 
 setTimeout(function() {
     if (process.platform === "win32") {
-        spawn("taskkill", ["/pid", liveServer.pid, '/f', '/t']);
+        spawn("taskkill", ["/pid", liveServer.pid, '/f', '/t'])
+            .on('close', function() {
+                console.log('exit code: ', pass ? '0' : '1');
+                process.exit(pass ? 0 : 1);
+            });
     } else {
         process.kill(-liveServer.pid, 'SIGTERM');
     }
-}, 30000);
+}, 20000);
